05fc2640b9a220eb1602a730574bfbcd46c7bc92
Kerry Imai <kc815hk@hotmail.com>
2016-02-18 15:38:46 -0500

    complete create message form and routes

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,12 +1,32 @@
 class MessagesController < ApplicationController
 
   def new
-    @message = Message.new
+    @user = User.find(params[:user_id])
+    @message = @user.messages.build
   end
 
   def create
+    @user = User.find(params[:user_id])
+    @message = @user.messages.build(msg_params)
+    if @message.save
+      redirect_to user_path(@user)
+    end
   end
 
 
-
+  private
+  def msg_params
+    params.require(:message).permit(:content, :to_user).merge(from_user: current_user)
+  end
 end
+
+
+def create
+    @dog = Dog.find(params[:dog_id])
+    @message = Message.new(msg_params)
+    if @message.save
+      redirect_to "/dogs/#{@dog.id}"
+    else
+      render :new
+    end
+  end
