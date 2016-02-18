a0484fe9c019e13fe63b1b1d4a4600045226a74f
Kerry Imai <email": null>
2016-02-18 14:51:06 -0500

    Minor correction in routes file and add gem pub chat

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
 resources :users do
-  resources :messages, only [:index, :show, :new, :create, :destroy]
+  resources :messages, only: [:index, :show, :new, :create, :destroy]
   get '/inbox' => 'messages#index'
-  resources :reviews, only [:new, :create, :edit, :update :destroy]
+  resources :reviews, only: [:new, :create, :edit, :update, :destroy]
 end
 
 
-  resource :session, only [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
     get '/login' => 'sessions#new'
     get '/register' => 'users#new'
     get '/logout' => 'session#destroy'
