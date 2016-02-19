05fd91cbc128823ed6e252481def71ceaad71e13
Jenna Espezua & Kerry Imai & Kristie Chow <pair+espezua.kerryimai.kristiechow@devbootcamp.com>
2016-02-18 20:55:41 -0500

    Create photos controller, modify routes & photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,8 @@
 class Photo < ActiveRecord::Base
+  has_attached_file :image
+  belongs_to :photoable, polymorphic: true
+
+  validates_attachment :image, presence: true,
+                     content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] },
+                     size: { in: 0..500.kilobytes }
 end
