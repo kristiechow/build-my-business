af3a00a6d50d14812b2dd156cd84ceb6e7de1346
Kerry Imai <kc815hk@hotmail.com>
2016-02-18 19:42:45 -0500

    add logged in method to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+    def user_signed_in?
+      if current_user
+        true
+      else
+       false
+      end
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160218221817) do
+ActiveRecord::Schema.define(version: 20160218232527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,43 +30,26 @@ ActiveRecord::Schema.define(version: 20160218221817) do
 
   add_index "businesses", ["owner_id"], name: "index_businesses_on_owner_id", using: :btree
 
-  create_table "developers", force: :cascade do |t|
-    t.string   "first_name",      null: false
-    t.string   "last_name",       null: false
-    t.string   "email",           null: false
-    t.string   "password_digest", null: false
-    t.string   "description"
-    t.string   "avatar"
-    t.string   "location"
-    t.string   "contact_info"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.string   "content",      null: false
-    t.integer  "from_user_id"
-    t.integer  "to_user_id"
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
 
-  add_index "messages", ["from_user_id"], name: "index_messages_on_from_user_id", using: :btree
-  add_index "messages", ["to_user_id"], name: "index_messages_on_to_user_id", using: :btree
+  add_index "conversations", ["recipient_id"], name: "index_conversations_on_recipient_id", using: :btree
+  add_index "conversations", ["sender_id"], name: "index_conversations_on_sender_id", using: :btree
 
-  create_table "owners", force: :cascade do |t|
-    t.string   "first_name",      null: false
-    t.string   "last_name",       null: false
-    t.string   "email",           null: false
-    t.string   "password_digest", null: false
-    t.string   "description"
-    t.string   "skills"
-    t.string   "avatar"
-    t.string   "location"
-    t.string   "contact_info"
+  create_table "messages", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "conversation_id"
+    t.integer  "user_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
+
+  add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
 
   create_table "reviews", force: :cascade do |t|
     t.string   "comment",              null: false
@@ -84,17 +67,18 @@ ActiveRecord::Schema.define(version: 20160218221817) do
   add_index "reviews", ["reviewer_id"], name: "index_reviews_on_reviewer_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",           null: false
-    t.string   "password_digest", null: false
-    t.string   "description",     null: false
-    t.string   "skills"
-    t.string   "avatar"
-    t.string   "location"
-    t.string   "contact_info",    null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
     t.string   "first_name",      null: false
     t.string   "last_name",       null: false
+    t.string   "email",           null: false
+    t.string   "password_digest", null: false
+    t.string   "description"
+    t.string   "skills"
+    t.string   "location"
+    t.string   "type"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
 end
