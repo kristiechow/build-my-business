62d581cc3a3c0097fcc586b803de40daaf219c8a
Kristie Chow <kristiemhchow@gmail.com>
2016-02-20 16:25:58 -0500

    Add business seed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220225621) do
+ActiveRecord::Schema.define(version: 20160220210422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20160220225621) do
     t.integer  "owner_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.float    "lat"
+    t.float    "lng"
   end
 
   add_index "businesses", ["owner_id"], name: "index_businesses_on_owner_id", using: :btree
