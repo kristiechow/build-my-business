ef25662ba2cab8237954b088a49f1da779b72b92
Jenna Espezua <jennaespezua@gmail.com>
2016-02-20 13:10:37 -0500

    Update seeds file to create unique uid

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-git # This file should contain all the record creation needed to seed the database with its default values.
+# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
@@ -6,9 +6,9 @@ git # This file should contain all the record creation needed to seed the databa
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 10.times do
-  Developer.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "123123", uid: "123456789", provider: "facebook")
+  Developer.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "123123", uid: SecureRandom.uuid , provider: "facebook")
 end
 
 10.times do
-  Owner.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "123123", uid: "123456789", provider: "facebook")
+  Owner.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "123123", uid: SecureRandom.uuid, provider: "facebook")
 end
