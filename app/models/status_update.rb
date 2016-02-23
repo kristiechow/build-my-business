01ee4efbddc2bb801cec7ce960459bada90e3d3a
Kristie Chow <kristiemhchow@gmail.com>
2016-02-23 18:51:04 -0500

    Complete status updates for businesses

--- app/models/status_update.rb
+++ app/models/status_update.rb
@@ -1,4 +1,10 @@
 class StatusUpdate < ActiveRecord::Base
   belongs_to :business
   has_many :photos
+
+  def create_photos(photographs)
+    photographs.each do |image|
+      self.photos.create(image: image)
+    end
+  end
 end
