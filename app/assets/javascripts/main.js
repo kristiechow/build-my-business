14f707a33906b6dad80c28b5156a9fe01661bbd1
Kerry Imai <email": null>
2016-02-23 11:29:51 -0500

    Add Jquery plugin for fade in and out

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $(".animsition").animsition({
+    inClass: 'fade-in-up',
+    outClass: 'fade-out-right-lg',
+    linkElement: 'a',
+    inDuration: 1000,
+    outDuration: 500
+  });
+
+
+
+})
