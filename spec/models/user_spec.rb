f602e82f4faa6ed523a8ba26a1a8224f03be9c88
Jenna Espezua <jennaespezua@gmail.com>
2016-02-24 11:44:51 -0500

    Update for testing user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context 'ActiveModel Validations' do
+    let(:user) {FactoryGirl.build :user}
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :uid }
+    it { should validate_uniqueness_of :uid }
+  end
+
 end
