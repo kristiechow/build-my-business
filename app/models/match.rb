d78b3279280c6e6fc1d992a5c2365bd7c487dc63
Kerry Imai <kmc736@nyu.edu>
2016-02-24 11:39:40 -0500

    Complete business status update option in progress

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,14 @@
 class Match < ActiveRecord::Base
   belongs_to :user
   belongs_to :matched_user, class_name: "User"
+
+
+  def update_status
+    if self.user.type == "owner"
+      self.user.business.update_attributes(status: "In progress")
+    else
+      self.matched_user.business.update_attributes(status: "In progress")
+    end
+  end
+
 end
