fba22c8f3b25dd56f12f07128c0df23c297159de
Jenna Espezua <jennaespezua@gmail.com>
2016-02-24 14:47:37 -0500

    Delete contact info models and migrations

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  it {should have_and_belong_to_many :businesses}
+  describe 'ActiveRecord associations' do
+    it {should have_and_belong_to_many :businesses}
+  end
 end
